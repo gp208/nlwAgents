b44a7fe754e49fafa530e8a54ed0948936f6e978
gp208 <gustavocprieto@gmail.com>
2025-07-10 18:47:22 -0300

    feat: list rooms

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CreateRoom } from './pages/createRoom';
 import { Room } from './pages/room';
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<CreateRoom/>} />
-        <Route path='/room' element={<Room/>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoom/>} />
+          <Route path='/room/:roomId' element={<Room/>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,3 +1,11 @@
+import { Navigate, useParams } from 'react-router-dom'
+
+type RoomParams = {
+  roomId: string
+}
+
 export function Room() {
-  return <div>Room details</div>
+  const params = useParams<RoomParams>()
+  if (!params.roomId) return <Navigate to='/' replace/>
+  return <div>Room details {JSON.stringify(params)}</div>
 }
