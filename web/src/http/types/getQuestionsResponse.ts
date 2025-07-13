a57c2433b2ac18ca50082726f367677a12aca786
gp208 <gustavocprieto@gmail.com>
2025-07-13 11:46:30 -0300

    improved question creation interface

--- web/src/http/types/getQuestionsResponse.ts
+++ web/src/http/types/getQuestionsResponse.ts
@@ -3,4 +3,5 @@ export type GetQuestionsResponse = Array<{
   question: string
   answer: string | null
   createdAt: string
+  isGeneratingAnswer?: boolean
 }>
--- web/src/http/useCreateQuestion.ts
+++ web/src/http/useCreateQuestion.ts
@@ -1,6 +1,7 @@
 import {useMutation, useQueryClient} from '@tanstack/react-query'
 import type {CreateQuestionRequest} from './types/createQuestionRequest'
 import type {CreateQuestionResponse} from './types/createQuestionResponse'
+import type {GetQuestionsResponse} from './types/getQuestionsResponse'
 
 export function useCreateQuestion(roomId: string) {
   const queryClient = useQueryClient()
@@ -15,6 +16,42 @@ export function useCreateQuestion(roomId: string) {
       const result: CreateQuestionResponse = await response.json()
       return result
     },
-    onSuccess: () => {queryClient.invalidateQueries({queryKey: ['getQuestions']})}
+    onMutate({question}) {
+      const questions = queryClient.getQueryData<GetQuestionsResponse>(['getQuestions', roomId])
+      const questionsArray = questions ?? []
+      const newQuestion = {
+        id: crypto.randomUUID(),
+        question,
+        answer: null,
+        createdAt: new Date().toISOString(),
+        isGeneratingAnswer: true
+      }
+      queryClient.setQueryData<GetQuestionsResponse>(
+        ['getQuestions', roomId],
+        [newQuestion, ...questionsArray]
+      )
+
+      return {newQuestion, questions}
+    },
+    onSuccess(data, _variables, context) {
+      queryClient.setQueryData<GetQuestionsResponse>(
+        ['getQuestions', roomId],
+        questions => {
+          if(!questions || !context.newQuestion) return questions
+          return questions.map(question => {
+            if(question.id == context.newQuestion.id)
+              return {...context.newQuestion, id: data.questionId, answer: data.answer, isGeneratingAnswer: false}
+            return question
+          })
+        }
+      )
+    },
+    onError(_data, _variables, context) {
+      if(context?.questions) queryClient.setQueryData<GetQuestionsResponse>(
+        ['getQuestions', roomId],
+        context.questions
+      )
+    }
+    //onSuccess: () => {queryClient.invalidateQueries({queryKey: ['getQuestions']})}
   })
 }
